3b0eb80f481f3b7e5d6248728aae8be365129c4c
Lucas <lucas.rosenorn@outlook.dk>
2026-01-11 18:12:15 +0100

    Task: Add lint ignore for only export component

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -55,6 +55,7 @@ export const ThemeProvider: React.FC<{ children: ReactNode }> = ({ children }) =
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useTheme = (): ThemeContextType => {
   const context = useContext(ThemeContext);
   if (!context) {
