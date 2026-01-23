a10ef34b32d08b6ed35d0e47e682cc2b736f3bcf
Lucas <lucas.rosenorn@outlook.dk>
2026-01-23 15:44:25 +0100

    Task: Add min height to main

--- src/App.tsx
+++ src/App.tsx
@@ -95,7 +95,7 @@ const AppWithAuthCheck: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-br from-base-200/90 via-base-300 to-primary/20">
       <Navbar />
-      <main className="flex-1">
+      <main className="flex-1 min-h-[70vh] pb-8">
         <AnimatePresence mode="wait" initial={false}>
           <Routes location={location} key={routeKey}>
             <Route path="/" element={<PageWrapper direction={direction} pathKey={routeKey}><Home /></PageWrapper>} />
